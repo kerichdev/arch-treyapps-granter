678d1e6e26d7773f7045a767b6344ba5931ea132
kerichuu <50075292+kerichdev@users.noreply.github.com>
2020-11-02 15:02:24 +0300

    initial release

---
+++ granter-v4.sh
@@ -0,0 +1,28 @@
+package=android-tools
+if ! [ $(id -u) = 0 ]; then
+   echo "Please run this script as root (sudo)"
+   exit 1
+fi
+if pacman -Qs $package > /dev/null ; then
+  echo "Package $package is installed, skipping installation"
+else
+  echo "Package $package is not installed, installing as root, enter your password if prompted"
+  sudo pacman -S --noconfirm android-tools
+  clear
+fi
+echo "Please connect your phone."
+adb wait-for-device
+clear
+echo "Checking deive permissions..."
+adb devices
+echo "If you see 'no permissions', please make sure you enabled usb debugging, in your device dev settings, set the usb mode to 'File Transfer', and granted the device dubug in a promt on your phone, then run the script again. If not, press enter."
+read
+echo "Proceeding..."
+echo "Detecting and granting the shades..."
+adb shell pm grant com.treydev.mns android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.pns android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.ons android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.micontrolcenter
+android.permission.WRITE_SECURE_SETTINGS
+clear
+echo "All done!"
