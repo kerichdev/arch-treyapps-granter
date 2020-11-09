630befe0a17544f67a7e313978c0370ac4ae3101
kerichuu <50075292+kerichdev@users.noreply.github.com>
2020-11-09 15:22:41 +0300

    Update granter
    [NEW] Added a note to enable USB debugging before checking for devices
    [NEW] Added a note for ColorOS (Oppo) users to disable permission monitoring
    [FIX] Formatting and grammar fixes

--- granter.sh
+++ granter.sh
@@ -10,7 +10,7 @@ else
   sudo pacman -S --noconfirm android-tools
   clear
 fi
-echo "Please connect your phone."
+echo "Please connect your phone. Make sure USB debugging is turned on in Developer settings. If you are a ColorOS (Oppo) user, please disable permission monitoring in the same section."
 adb wait-for-device
 clear
 echo "Checking deive permissions..."
@@ -22,7 +22,6 @@ echo "Detecting and granting the shades..."
 adb shell pm grant com.treydev.mns android.permission.WRITE_SECURE_SETTINGS
 adb shell pm grant com.treydev.pns android.permission.WRITE_SECURE_SETTINGS
 adb shell pm grant com.treydev.ons android.permission.WRITE_SECURE_SETTINGS
-adb shell pm grant com.treydev.micontrolcenter
-android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.micontrolcenter android.permission.WRITE_SECURE_SETTINGS
 clear
-echo "All done!"
+echo "All done! You can disconnect your device now."
