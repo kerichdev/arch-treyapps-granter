a68ec7c9e6cacffe0976e838e55df3676e91f906
kerichuu <50075292+kerichdev@users.noreply.github.com>
2020-11-10 15:38:56 +0300

    Update granter.sh
    [DONE] Reimplement android-tools installation, now with package verifying, and error stop
    [FIX] In case of an error, the terminal won't be cleaned, so the user can review the error

--- granter.sh
+++ granter.sh
@@ -1,2 +1,24 @@
 devmodel=$(adb shell getprop ro.product.manufacturer)
 adpackage=android-tools
+
+if ! [ $(id -u) = 0 ]; then
+   echo "Please run this script as root (same command you did but with sudo at beginning)"
+   exit 1
+fi
+
+if pacman -Qs $adpackage > /dev/null ; then
+  echo "android-tools are installed, skipping installation"
+else
+  echo "android-tools are not installed, installing as root, enter your password if prompted"
+  sudo pacman -S --noconfirm android-tools
+  retval=$?
+  if [ $retval -ne 0 ]; then
+    echo "Pacman returned $retval, please review the installation log and try again."
+    exit 1
+  else
+    echo "Pacman returned no errors, verifying installation..."
+    if pacman -Qs $adpackage > /dev/null ; then
+      echo "Installation successful, proceeding..."
+      clear
+    fi
+fi
