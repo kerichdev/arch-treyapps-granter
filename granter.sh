b412d709439bc9f7e73ae2ff0cd34f9c18eae17f
kerichuu <50075292+kerichdev@users.noreply.github.com>
2020-11-11 18:06:14 +0300

    Update granter.sh
    Granter v2 - rewritten from scratch, improved beyond impossible.
    (Release v2.0 testing, soon will be public)
    
    [NEW] Reimplement permission granting, aka the core function of this script
    [FIX] Device connectivity checker works now
    [NEW] Device connectivity checker is just one line now, thanks to @KenHV for this
    [IN-DEV] Code will be cleaned up soon

--- granter.sh
+++ granter.sh
@@ -31,6 +31,8 @@ fi
 echo "Please connect your phone. Make sure USB debugging is turned on in Developer settings. If you're stuck at this step, switch USB mode to File transfer."
 adb wait-for-device
 echo "Device connected, fetching info..."
+
+#OLD CODE FOR DEVICE CHECKING
 #if ! [ $(adb devices | awk 'NR==2 {print $2}') = "device" ]; then
  #echo "Proceeding..."
 #else
@@ -38,7 +40,8 @@ echo "Device connected, fetching info..."
  #exit 1
 #fi
 
-#fix here soon
+#NEW CODE FOR DEVICE CHECKING
+[ $awkcheck = device ] && echo "Proceeding..." || echo "Device not connected properly. Please visit https://github.com/kerichdev/arch-treyapps-granter/blob/v2/guide.txt for a fix"
 
 if echo $(adb shell getprop ro.product.manufacturer) | grep -q -i "OPPO"; then
  echo "OPPO device found, please make sure you have disabled permission monitor in developer settings, then press enter."
@@ -46,4 +49,13 @@ if echo $(adb shell getprop ro.product.manufacturer) | grep -q -i "OPPO"; then
 elif echo $(adb shell getprop ro.product.manufacturer) | grep -q -i "Huawei"; then
  echo "Huawei device found, please note that the apps may not work as stable as expected due to system restrictions, and it's not the developer's fault. Press enter to continue."
  read 
+else
+ echo $(adb shell getprop ro.product.manufacturer)" device found, all good..."
 fi
+echo "Granting the shades..."
+adb shell pm grant com.treydev.mns android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.pns android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.ons android.permission.WRITE_SECURE_SETTINGS
+adb shell pm grant com.treydev.micontrolcenter android.permission.WRITE_SECURE_SETTINGS
+clear
+echo "Granted successfully, you can disconnect your device now."
