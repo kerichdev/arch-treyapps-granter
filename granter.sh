b399963c6707b3dd250ab29130015cc07419481d
Keri <50075292+kerichdev@users.noreply.github.com>
2022-08-21 20:24:59 +0300

    granter.sh: touch-ups and fixes
    code compacting, removed some unnecessary checks and lines.

--- granter.sh
+++ granter.sh
@@ -5,34 +5,30 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 if pacman -Qs $package > /dev/null ; then
-  echo "android-tools are installed, skipping installation"
+  echo "'android-tools' package is installed, skipping..."
 else
-  echo "android-tools are not installed, installing as root, enter your password if prompted"
+  echo "'android-tools' package is not installed, installing..."
   sudo pacman -S --noconfirm android-tools
   retval=$?
   if [ $retval -ne 0 ]; then
-    echo "Pacman returned $retval, please review the installation log and try again."
+    echo "Installation failed with code $retval, please try again."
     exit 1
   else
-    echo "Pacman returned no errors, verifying installation..."
-    if pacman -Qs $package > /dev/null ; then
-      echo "Installation successful, proceeding..."
-      clear
-    fi
+    echo "Pacman returned no errors, proceeding..."
   fi
 fi
 echo "Please connect your phone. Make sure USB debugging is turned on in Developer settings. If you're stuck at this step, switch USB mode to File transfer."
 adb wait-for-device
 echo "Device connected, fetching info..."
-[ $awkcheck = device ] && echo "Proceeding..." || echo "Device not connected properly. Please visit https://github.com/kerichdev/arch-treyapps-granter/blob/v2/guide.txt for a fix"
+[ $awkcheck = device ] && echo "Proceeding..." || echo "Device not connected properly."
 if echo $(adb shell getprop ro.product.manufacturer) | grep -q -i "OPPO"; then
  echo "OPPO device found, please make sure you have disabled permission monitor in developer settings, then press enter."
  read 
 elif echo $(adb shell getprop ro.product.manufacturer) | grep -q -i "Huawei"; then
- echo "Huawei device found, please note that the apps may not work as stable as expected due to system restrictions, and it's not the developer's fault. Press enter to continue."
+ echo "Huawei device found, please note that the apps may not work as expected due to system restrictions. Press enter to continue."
  read 
 else
- echo $(adb shell getprop ro.product.manufacturer)" device found, all good..."
+ echo $(adb shell getprop ro.product.manufacturer)" device found."
 fi
 echo "Granting the shades..."
 adb shell pm grant com.treydev.mns android.permission.WRITE_SECURE_SETTINGS
@@ -40,4 +36,4 @@ adb shell pm grant com.treydev.pns android.permission.WRITE_SECURE_SETTINGS
 adb shell pm grant com.treydev.ons android.permission.WRITE_SECURE_SETTINGS
 adb shell pm grant com.treydev.micontrolcenter android.permission.WRITE_SECURE_SETTINGS
 clear
-echo "Granted successfully, you can disconnect your device now."
+echo "Permissions granted, you can disconnect your device now."
